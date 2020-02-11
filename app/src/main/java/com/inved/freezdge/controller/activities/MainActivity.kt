6b707d889dbd2de9315f25603df53079db367268
inveders <alexandra.gnimadi@gmail.com>
2020-02-11 11:34:20 +0100

    Initial commit

---
+++ app/src/main/java/com/inved/freezdge/controller/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.inved.freezdge.controller.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inved.freezdge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
