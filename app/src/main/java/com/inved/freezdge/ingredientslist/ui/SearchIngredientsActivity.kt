74d3afb90e93415ff4b263dd73aee89ba54206d7
Alexandra Gnimadi <alexandra.gnimadi@gmail.com>
2020-04-15 08:40:30 +0200

    Implementing searchview in search Ingredient activity

--- app/src/main/java/com/inved/freezdge/ingredientslist/ui/SearchIngredientsActivity.kt
+++ app/src/main/java/com/inved/freezdge/ingredientslist/ui/SearchIngredientsActivity.kt
@@ -2,9 +2,10 @@ package com.inved.freezdge.ingredientslist.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
-import android.widget.Toast
-import androidx.annotation.Nullable
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,16 +14,13 @@ import com.inved.freezdge.R
 import com.inved.freezdge.ingredientslist.database.Ingredients
 import com.inved.freezdge.ingredientslist.viewmodel.IngredientsViewModel
 import com.inved.freezdge.uiGeneral.activity.BaseActivity
-import com.inved.freezdge.utils.AddIngredientsInDatabase
-import com.inved.freezdge.utils.App
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.IAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
-import io.objectbox.Box
-import io.objectbox.kotlin.boxFor
+import java.util.*
 
 
-class SearchIngredientsActivity: BaseActivity() {
+class SearchIngredientsActivity : BaseActivity() {
 
     private val foodSearchItemAdapter = ItemAdapter<Ingredients>()
     private val fastAdapterFoodSearch = FastAdapter.with(foodSearchItemAdapter)
@@ -41,10 +39,49 @@ class SearchIngredientsActivity: BaseActivity() {
         setupRecyclerView()
         getAllFood()
 
-
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu?.findItem(R.id.search_menu)
+        if (searchItem != null) {
+            val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
+            val edittext=searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+            edittext.hint="TBL: Search ingredient here..."
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+
+                    if (newText!!.isNotEmpty()) {
+
+                        foodSearchItemAdapter.clear()
+                        val search = newText.toLowerCase(Locale.ROOT)
+                        ingredientViewmodel.getAllIngredients()
+                            .observe({ lifecycle }, { list ->
+
+                                list.forEach {
+                                    if (it.name!!.toLowerCase(Locale.ROOT).contains(search)) {
+                                        foodSearchItemAdapter.add(it)
+                                    }
+                                }
+
+                            })
+
+                    } else {
+                        getAllFood()
+                    }
+
+                    return true
+                }
+            })
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
 
     override fun getLayoutContentViewID(): Int {
         return R.layout.activity_search_ingredients
@@ -59,14 +96,15 @@ class SearchIngredientsActivity: BaseActivity() {
         recyclerView.adapter = fastAdapterFoodSearch
 
         //configure our fastAdapter
-        fastAdapterFoodSearch.onClickListener = { v: View?, _: IAdapter<Ingredients>, item: Ingredients, _: Int ->
-            v?.let {
-                Log.d("debago","item is ${item.selectedIngredient}")
-                ingredientViewmodel.updateIngredient(item)
-                fastAdapterFoodSearch.notifyAdapterDataSetChanged()
+        fastAdapterFoodSearch.onClickListener =
+            { v: View?, _: IAdapter<Ingredients>, item: Ingredients, position: Int ->
+                v?.let {
+                    Log.d("debago", "item is ${item.selectedIngredient}")
+                    ingredientViewmodel.updateIngredient(item)
+                    fastAdapterFoodSearch.notifyAdapterItemChanged(position)
+                }
+                false
             }
-            false
-        }
 
     }
 
@@ -78,6 +116,7 @@ class SearchIngredientsActivity: BaseActivity() {
 
 
     fun getAllFood() {
+        foodSearchItemAdapter.clear()
         ingredientViewmodel.getAllIngredients().observe(this, Observer {
             foodSearchItemAdapter.add(it)
         })
