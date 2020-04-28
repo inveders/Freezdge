dbce9406e338581223d3a6bf595383eb97e97593
Alexandra Gnimadi <alexandra.gnimadi@gmail.com>
2020-04-29 01:15:20 +0200

    Adjust photo dialog, split in two button, one for gallery, one for camera

--- app/src/main/java/com/inved/freezdge/socialmedia/ui/SocialMediaFragment.kt
+++ app/src/main/java/com/inved/freezdge/socialmedia/ui/SocialMediaFragment.kt
@@ -1,6 +1,8 @@
 package com.inved.freezdge.socialmedia.ui
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,23 +13,32 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.Query
 import com.inved.freezdge.R
 import com.inved.freezdge.socialmedia.firebase.Post
 import com.inved.freezdge.socialmedia.firebase.PostHelper
+import com.inved.freezdge.socialmedia.firebase.User
+import com.inved.freezdge.socialmedia.firebase.UserHelper
 import com.inved.freezdge.socialmedia.view.PostsAdapter
-import kotlinx.android.synthetic.main.fragment_social_media.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
-class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
+@RuntimePermissions
+class SocialMediaFragment: Fragment() {
 
     private lateinit var mRecyclerPostsAdapter:PostsAdapter
     private lateinit var recyclerView: RecyclerView
-    private lateinit var addPhotoImage: ImageView
+    private lateinit var addPhotoGallery: ImageView
+    private lateinit var addPhotoCamera: ImageView
     private lateinit var addTipImage: ImageView
     private lateinit var photoProfile: ImageView
-    private lateinit var addPhotoText: TextView
+    private lateinit var addPhotoGalleryText: TextView
+    private lateinit var addPhotoCameraText: TextView
     private lateinit var addTipText: TextView
+    private lateinit var topDescription: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,31 +46,84 @@ class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
     ): View? {
         val mView: View = inflater.inflate(R.layout.fragment_social_media, container, false)
         recyclerView= mView.findViewById(R.id.recyclerview)
-        addPhotoImage=mView.findViewById(R.id.image_photo_post)
+        addPhotoGallery=mView.findViewById(R.id.image_photo_post)
+        addPhotoCamera=mView.findViewById(R.id.image_camera_post)
         addTipImage=mView.findViewById(R.id.image_tips_post)
-        addPhotoText=mView.findViewById(R.id.title_photo_post)
+        addPhotoGalleryText=mView.findViewById(R.id.title_photo_post)
+        addPhotoCameraText=mView.findViewById(R.id.title_camera_post)
         addTipText=mView.findViewById(R.id.title_tips_post)
         photoProfile=mView.findViewById(R.id.profile_image)
+        topDescription=mView.findViewById(R.id.profile_activity_top_description)
         //RecyclerView initialization
         displayAllPosts()
         initButtons()
+        initProfil()
         return mView
     }
 
+    override fun onResume() {
+        super.onResume()
+        displayAllPosts()
+    }
+
+    private fun initProfil() {
+
+        UserHelper.getUser(FirebaseAuth.getInstance().currentUser?.uid)?.get()?.addOnCompleteListener { task ->
+            if (task.result != null) {
+                if (task.result!!.documents.isNotEmpty()) {
+
+                    val user: User =
+                        task.result!!.documents[0].toObject(User::class.java)!!
+
+                    topDescription.text = getString(R.string.social_media_description,user.firstname)
+
+                    //to upload a photo on Firebase storage
+                    if(user.photoUrl!=null){
+                        photoProfile.let {
+                            activity?.let { it1 ->
+                                Glide.with(it1)
+                                    .load(user.photoUrl)
+                                    .apply(RequestOptions.circleCropTransform())
+                                    .placeholder(R.drawable.ic_anon_user_48dp)
+                                    .into(it)
+                            }
+                        }
+                    }else{
+                        photoProfile.let {
+                            activity?.let { it1 ->
+                                Glide.with(it1)
+                                    .load(R.drawable.ic_anon_user_48dp)
+                                    .apply(RequestOptions.circleCropTransform())
+                                    .placeholder(R.drawable.ic_anon_user_48dp)
+                                    .into(it)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }?.addOnFailureListener { e ->
+            Log.e(
+                "debago",
+                "Problem during the user creation"
+            )
+        }
+    }
+
     private fun initButtons() {
-        addPhotoImage.setOnClickListener { onClickAddPhoto() }
-        addPhotoText.setOnClickListener { onClickAddPhoto() }
+        addPhotoCamera.setOnClickListener { onClickAddPhotoWithPermissionCheck() }
+        addPhotoCameraText.setOnClickListener { onClickAddPhotoWithPermissionCheck() }
+        addPhotoGallery.setOnClickListener { onClickAddPhotoGalleryWithPermissionCheck() }
+        addPhotoGalleryText.setOnClickListener { onClickAddPhotoGalleryWithPermissionCheck() }
         addTipImage.setOnClickListener { onClickAddTips() }
         addTipText.setOnClickListener { onClickAddTips() }
         photoProfile.setOnClickListener { onClickUpdateProfil() }
     }
 
     private fun displayAllPosts() {
+        Log.d("debago","in display all posts")
         mRecyclerPostsAdapter = PostsAdapter(
-            generateOptionsForAdapter(PostHelper.getAllPosts()),
-            Glide.with(this),
-            this,
-            activity
+            generateOptionsForAdapter(PostHelper.getAllPosts())
         )
         //Choose how to display the list in the RecyclerView (vertical or horizontal)
         recyclerView.setHasFixedSize(true)
@@ -68,24 +132,21 @@ class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
             RecyclerView.VERTICAL,
             false
         )
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                activity,
-                DividerItemDecoration.VERTICAL
-            )
-        )
+
         recyclerView.adapter = mRecyclerPostsAdapter
     }
 
     // Create options for RecyclerView from a Query
     private fun generateOptionsForAdapter(query: Query): FirestoreRecyclerOptions<Post?> {
+        Log.d("debago","in generate options for adapter")
         return FirestoreRecyclerOptions.Builder<Post>()
             .setQuery(query, Post::class.java)
             .setLifecycleOwner(this)
             .build()
     }
 
-    private fun onClickAddPhoto(){
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun onClickAddPhoto(){
         val transaction = activity?.supportFragmentManager?.beginTransaction()
         val previous = activity?.supportFragmentManager?.findFragmentByTag(PhotoDialog.TAG)
         if (previous != null) {
@@ -93,11 +154,27 @@ class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
         }
         transaction?.addToBackStack(null)
 
-        val dialogFragment = PhotoDialog.newInstance("parameter")
+        val dialogFragment = PhotoDialog.newInstance(1)
         if (transaction != null) {
             dialogFragment.show(transaction, PhotoDialog.TAG)
         }
     }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    fun onClickAddPhotoGallery(){
+        val transaction = activity?.supportFragmentManager?.beginTransaction()
+        val previous = activity?.supportFragmentManager?.findFragmentByTag(PhotoDialog.TAG)
+        if (previous != null) {
+            transaction?.remove(previous)
+        }
+        transaction?.addToBackStack(null)
+
+        val dialogFragment = PhotoDialog.newInstance(2)
+        if (transaction != null) {
+            dialogFragment.show(transaction, PhotoDialog.TAG)
+        }
+    }
+
 
     private fun onClickAddTips(){
         val transaction = activity?.supportFragmentManager?.beginTransaction()
@@ -113,7 +190,8 @@ class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
         }
     }
 
-    private fun onClickUpdateProfil(){
+
+    fun onClickUpdateProfil(){
         val transaction = activity?.supportFragmentManager?.beginTransaction()
         val previous = activity?.supportFragmentManager?.findFragmentByTag(ProfilDialog.TAG)
         if (previous != null) {
@@ -127,16 +205,32 @@ class SocialMediaFragment: Fragment(),PostsAdapter.Listener {
         }
     }
 
+
+
+    override fun onRequestPermissionsResult(rc: Int, permissions: Array<out String>, results: IntArray) {
+        // Java: "MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, rc, results);"
+        // I'm not satisfied with the method signature here, since it's too similar to the Android one.
+        // However, the signature is already pretty long, so I'm open for ideas.
+        this.onRequestPermissionsResult(rc, results)
+    }
+
+
+
+
+
     // --------------------
     // CALLBACK
     // --------------------
-    override fun onDataChanged() {
+    /*override fun onDataChanged() {
         // 7 - Show TextView in case RecyclerView is empty
+        Log.d("debago","in on data changed ${mRecyclerPostsAdapter.itemCount}")
         if(mRecyclerPostsAdapter.itemCount==0){
             no_post_found.visibility=View.VISIBLE
         }else{
             no_post_found.visibility=View.GONE
         }
-    }
+    }*/
+
+
 
 }
